273c8b0eb787e948bd9af08f48322fb512e7645d
thanhlong0728 <thanhlong0728@gmail.com>
2025-03-27 22:54:44 +0700

    first commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Mac on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
